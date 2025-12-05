e3fd29bc55d6eae893cf1667c0d6d58b6505e791
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2025-12-05 16:16:55 +0700

    VinDr CXR training

--- MedSight3/src/dataloader.py
+++ MedSight3/src/dataloader.py
@@ -115,6 +115,7 @@ def get_dataloaders(train_csv, test_csv, train_dir, test_dir, batch_size=32, ran
     """
     # Augmentation cho MedMAE (Input 224x224)
     train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize về 224x224 trước
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
         transforms.ColorJitter(brightness=0.2, contrast=0.2), # Contrast rất quan trọng với X-ray
@@ -123,6 +124,7 @@ def get_dataloaders(train_csv, test_csv, train_dir, test_dir, batch_size=32, ran
     ])
     
     val_transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize về 224x224
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
